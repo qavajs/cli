3da7004031e1eaa8f141d418a39ae63d63b06a0b
Alexander Galichenko <alexandr.galichenko@gmail.com>
2022-06-20 13:44:09 +0300

    Service support (#5)
    * added extension point to handle global hooks
    
    * added readme
    
    * added tupple configuration
    
    * fixed bug that fail run if service property is not set
    
    Co-authored-by: Oleksandr_Halichenko <oleksandr_halichenko@epam.com>

---
+++ src/ServiceHandler.ts
@@ -0,0 +1,50 @@
+import path from 'path';
+
+export default class ServiceHandler {
+    private config: Config;
+    readonly services: Array<Service>;
+    constructor(configPath: string, profile: string) {
+        this.config = require(path.join(process.cwd(), configPath))[profile];
+        this.services = this.loadServices();
+    }
+
+    loadServices() {
+        if (!this.config.service) return [];
+        return this.config.service.map(svc => {
+            if (typeof svc === 'string') {
+                try {
+                    require.resolve(svc);
+                    return require(svc)
+                } catch (e) {
+                    return require(path.join(process.cwd(), svc))
+                }
+            }
+            else if (Array.isArray(svc)) {
+                const [svcPath, options] = svc;
+                let service;
+                try {
+                    require.resolve(svcPath);
+                    service = require(svcPath)
+                } catch (e) {
+                    service = require(path.join(process.cwd(), svcPath))
+                }
+                service.options = options;
+            }
+            else {
+                return svc
+            }
+        })
+    }
+
+    async before() {
+        for (const svc of this.services) {
+            if (svc.before) return svc.before();
+        }
+    }
+
+    async after() {
+        for (const svc of this.services) {
+            if (svc.after) return svc.after();
+        }
+    }
+}
