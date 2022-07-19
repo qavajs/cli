9b69de48f256221456dd98579c6784562562904e
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2022-07-19 19:22:16 +0300

    built-in config loader

--- src/run.ts
+++ src/run.ts
@@ -1,5 +1,6 @@
 import yargs from 'yargs';
 import ServiceHandler from './ServiceHandler';
+import path from 'path';
 
 export default async function(): Promise<void> {
     const { Cli } = await import('@cucumber/cucumber');
@@ -10,8 +11,9 @@ export default async function(): Promise<void> {
 
     const serviceHandler = new ServiceHandler(process.env.CONFIG as string, process.env.PROFILE as string);
     await serviceHandler.before();
+    const memoryLoadHook = path.resolve(process.cwd(), 'node_modules/@qavajs/lib/load_hook.js')
     const cli = new Cli({
-        argv: [...process.argv.slice(0,2), ...process.argv.slice(3)],
+        argv: [...process.argv.slice(0,2), '--require', memoryLoadHook, ...process.argv.slice(3)],
         cwd: process.cwd(),
         stdout: process.stdout,
         stderr: process.stderr,
