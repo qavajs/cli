64f56c6429a1451edcc2577d03c5465145d68156
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2022-07-21 16:21:53 +0300

    fix issue with default imports

--- src/wdioServiceAdapter.ts
+++ src/wdioServiceAdapter.ts
@@ -1,16 +1,15 @@
 export function wdioService(serviceDefinition: string | [string, object?, object?, object?]): Service {
     let servicePath = serviceDefinition;
-    let options;
-    let capabilities;
-    let config;
+    let options: object | undefined = {};
+    let capabilities: object | undefined = {};
+    let config: object | undefined = {};
     if (Array.isArray(serviceDefinition)) {
-        [servicePath, options = {}, capabilities = {}, config = {}] = serviceDefinition;
+        [servicePath, options, capabilities, config] = serviceDefinition;
     }
     const { launcher: ServiceClass } = require(servicePath as string);
-    console.log(ServiceClass)
     const service = new ServiceClass(options, capabilities, config);
-    service.before = service.onPrepare.bind(service, [config]);
-    service.after = service.onComplete.bind(service, [config]);
+    service.before = service.onPrepare.bind(service, [config, capabilities]);
+    service.after = service.onComplete.bind(service, [config, capabilities]);
     service.options = options;
     return service
 }
