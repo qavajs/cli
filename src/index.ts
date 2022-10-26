da916817a3c4d9939a7a3599ed8f3be5371184d6
Alexander Galichenko <alexandr.galichenko@gmail.com>
2022-10-26 10:22:51 +0300

    - updated run logic to use runCucumber() API instead of deprecated CLI() (#20)
    Co-authored-by: Oleksandr_Halichenko <oleksandr_halichenko@epam.com>

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import yargs from 'yargs';
 import install from './install';
-import cli from './run';
+import run from './run';
 
 const argv: any = yargs(process.argv.slice(2)).argv;
 
 if (!argv._[0]) console.warn('Specify command: install or run');
 switch (argv._[0]) {
     case 'install': install(); break;
-    case 'run': cli(); break;
+    case 'run': run(); break;
+    default: console.warn('Command is not supported');
 }
