6e4c1077771cf70defcf2c75800e7d0fe68d5ec2
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2023-01-17 20:47:15 +0300

    fixed windows issue with config import

--- src/importConfig.ts
+++ src/importConfig.ts
@@ -1,7 +1,7 @@
 import path from 'path';
 
 export default async function importConfig(configPath: string, profile: string): Promise<any> {
-    return import(path.join(process.cwd(), configPath)).then(config => config[profile]?.import
+    return import('file://' + path.join(process.cwd(), configPath)).then(config => config[profile]?.import
         ? config[profile]
         : config.default[profile]
     );
