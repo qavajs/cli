e837c299313ae921f3dbba46178732426de605b7
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2023-02-06 18:17:49 +0300

    added question to select commonjs / esm modules / typescript

--- src/install.ts
+++ src/install.ts
@@ -3,12 +3,14 @@ import fs from 'fs-extra';
 import path from 'path';
 import yarnInstall from 'yarn-install';
 import deps, {steps, format, modules, ModuleDefinition} from './deps';
+import ejs from 'ejs';
 
 type Answers = {
     steps: Array<string>,
     formats: Array<string>,
     modules: Array<string>,
-    parallel: number
+    parallel: number,
+    moduleSystem: string
 }
 
 const packs = (deps: Array<ModuleDefinition>) => deps.map(({module}) => module);
@@ -21,9 +23,21 @@ const packages = (moduleList: Array<string>, packageMap: Array<ModuleDefinition>
         }) as Array<string>
 }
 
+const replaceNewLines = (text: string) => text.replace(/\n+/g, '\n');
+
 export default async function install(): Promise<void> {
     const requiredDeps = [...deps];
     const answers = await inquirer.prompt([
+        {
+            type: 'list',
+            message: 'select module system you want to use:',
+            name: 'moduleSystem',
+            choices: [
+                'CommonJS',
+                'ES Modules',
+                'Typescript'
+            ]
+        },
         {
             type: 'checkbox',
             message: 'select step packages to install:',
@@ -48,6 +62,7 @@ export default async function install(): Promise<void> {
     const formatPackages: Array<string> = packages(answers.formats, format);
     const modulePackages: Array<string> = packages(answers.modules, modules);
 
+    const isTypescript = answers.moduleSystem === 'Typescript';
     const isWdioIncluded = answers.steps.includes('wdio');
     const isPlaywrightIncluded = answers.steps.includes('playwright');
     //checking if user selected only one browser driver
@@ -58,18 +73,23 @@ export default async function install(): Promise<void> {
     const isTemplateIncluded: boolean = answers.modules.includes('template');
 
     const configTemplate: string = await fs.readFile(
-        path.resolve(__dirname, '../templates/config.template'),
+        path.resolve(__dirname, '../templates/config.ejs'),
         'utf-8'
     );
-
-    let config: string = configTemplate
-        .replace('<steps>', JSON.stringify([...stepsPackages].map(p => 'node_modules/' + p)))
-        .replace('<format>', JSON.stringify(
+    const configEjs = ejs.compile(configTemplate);
+    const config = configEjs({
+        steps: JSON.stringify([...stepsPackages].map(p => 'node_modules/' + p)),
+        moduleSystem: answers.moduleSystem,
+        modules: JSON.stringify(modulePackages),
+        format: JSON.stringify(
             format
                 .filter(p => formatPackages.includes(p.packageName))
-                .map(p => p.packageName + (p.out ? `:${p.out}` : '')))
-        )
-        .replace('<modules>', JSON.stringify(modulePackages));
+                .map(p => p.packageName + (p.out ? `:${p.out}` : ''))
+        ),
+        isWdioIncluded,
+        isPlaywrightIncluded,
+        isTemplateIncluded
+    });
 
     await fs.ensureDir('./features');
     await fs.ensureDir('./memory');
@@ -79,51 +99,43 @@ export default async function install(): Promise<void> {
         const poModule = isWdioIncluded ? '@qavajs/po' : '@qavajs/po-playwright';
         requiredDeps.push(poModule);
         const featureTemplate: string = await fs.readFile(
-            path.resolve(__dirname, '../templates/feature.template'),
+            path.resolve(__dirname, '../templates/feature.ejs'),
             'utf-8'
         );
-        await fs.writeFile('./features/qavajs.feature', featureTemplate, 'utf-8');
-        const browserName = isWdioIncluded ? 'chrome' : 'chromium';
-        const pageObjectSnippet =
-    `
-        pageObject: new App(),
-        browser: {
-            capabilities: {
-                browserName: "${browserName}"
-            }
-        },
-    `
-        config = config
-            .replace('<importPageObject>', 'const App = require("./page_object");')
-            .replace('<configPageObject>', pageObjectSnippet);
+        const featureEjs = ejs.compile(featureTemplate);
+        const featureFile = featureEjs();
+        await fs.writeFile('./features/qavajs.feature', replaceNewLines(featureFile), 'utf-8');
 
         //create page object folder
         await fs.ensureDir('./page_object');
         const poTemplate: string = await fs.readFile(
-            path.resolve(__dirname, '../templates/po.template'),
+            path.resolve(__dirname, '../templates/po.ejs'),
             'utf-8'
         );
-        await fs.writeFile('./page_object/index.js', poTemplate.replace('<poModule>', poModule), 'utf-8');
+        const poEjs = ejs.compile(poTemplate);
+        const poFile = poEjs({
+            moduleSystem: answers.moduleSystem,
+            poModule
+        })
+        await fs.writeFile(`./page_object/index.${isTypescript ? 'ts' : 'js'}`, replaceNewLines(poFile), 'utf-8');
     }
 
     if (isTemplateIncluded) {
         await fs.ensureDir('./templates');
-        config = config
-            .replace('<templates>', 'templates: ["templates/*.feature"],');
     }
 
-    config = config
-        .replace(/<.+?>/g, '')
-        .replace(/\n\s+\n/g, '\n');
-
-    await fs.writeFile('config.js', config, 'utf-8');
+    await fs.writeFile(`config.${isTypescript ? 'ts' : 'js'}`, replaceNewLines(config), 'utf-8');
 
     const memoryTemplate: string = await fs.readFile(
-        path.resolve(__dirname, '../templates/memory.template'),
+        path.resolve(__dirname, '../templates/memory.ejs'),
         'utf-8'
     );
+    const memoryEjs = ejs.compile(memoryTemplate);
+    const memoryFile = memoryEjs({
+        moduleSystem: answers.moduleSystem
+    })
 
-    await fs.writeFile('./memory/index.js', memoryTemplate, 'utf-8');
+    await fs.writeFile(`./memory/index.${isTypescript ? 'ts' : 'js'}`, replaceNewLines(memoryFile), 'utf-8');
 
     const modulesToInstall = [...requiredDeps, ...stepsPackages, ...formatPackages, ...modulePackages];
     console.log('installing packages...');
