99cfc520884b9cbfcec571c9c34863ea9b2acd61
Alexander Galichenko <alexandr.galichenko@gmail.com>
2023-02-21 10:17:00 +0200

    added json computed (#41)

--- src/computed/file.ts
+++ src/computed/file.ts
@@ -2,5 +2,6 @@ import fs from 'fs/promises';
 
 export default {
     file: (path: string): Promise<Buffer> => fs.readFile(path),
-    textFile: (path: string): Promise<string> => fs.readFile(path, 'utf-8')
+    textFile: (path: string): Promise<string> => fs.readFile(path, 'utf-8'),
+    json: async (path: string): Promise<string> => JSON.parse(await fs.readFile(path, 'utf-8'))
 }
