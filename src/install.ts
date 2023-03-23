a4ef5a13f9d86cf78da4e0720ad0e241cd63102c
Alexander Galichenko <alexandr.galichenko@gmail.com>
2023-03-23 13:44:36 +0200

    updated steps imports generated by cli (#49)

--- src/install.ts
+++ src/install.ts
@@ -88,7 +88,7 @@ export default async function install(): Promise<void> {
     );
     const configEjs = ejs.compile(configTemplate);
     const config = configEjs({
-        steps: JSON.stringify([...stepsPackages].map(p => `node_modules/${p}`)),
+        steps: JSON.stringify([...stepsPackages].map(p => `node_modules/${p}/index.js`)),
         moduleSystem: answers.moduleSystem,
         modules: JSON.stringify(modulePackages),
         format: JSON.stringify(
