a689b1628f564630300c21a211c3c2baa621f503
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2023-05-04 10:48:58 +0300

    fixed support of multiple services

--- test-e2e-cjs/config.js
+++ test-e2e-cjs/config.js
@@ -11,6 +11,13 @@ module.exports = {
         parallel: 1,
         publishQuiet: true,
         service: [{
+            before() {
+                console.log('service 1 started');
+            },
+        }, {
+            before() {
+                console.log('service 2 started');
+            },
             after(result) {
                 console.log(result.success);
             }
--- test-e2e-esm/config.js
+++ test-e2e-esm/config.js
@@ -10,6 +10,13 @@ export default {
     parallel: 1,
     publishQuiet: true,
     service: [{
+        before() {
+            console.log('service 1 started');
+        },
+    }, {
+        before() {
+            console.log('service 2 started');
+        },
         after(result) {
             console.log(result.success);
         }
