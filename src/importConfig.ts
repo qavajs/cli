65388120a3fbd113ae1ef944490574ab0dd61b0c
Alexander Galichenko <alexandr.galichenko@gmail.com>
2023-10-10 15:22:06 +0300

    added native support of ts (#71)
    * added native support of ts
    * added support of CucumberJS 10

--- src/importConfig.ts
+++ src/importConfig.ts
@@ -1,9 +1,18 @@
-import {join} from 'path';
+import { join } from 'path';
 
+let loadTS = true;
+async function importTS(configPath: string) {
+    const { register } = require('ts-node');
+    if (loadTS) {
+        register();
+        loadTS = false;
+    }
+    return require(configPath)
+}
 export default async function importConfig(configPath: string, profile: string): Promise<any> {
     const fullPath = join(process.cwd(), configPath);
     const importer: Promise<any> = fullPath.endsWith('.ts')
-        ? Promise.resolve(require(fullPath))
+        ? importTS(fullPath)
         : import('file://' + fullPath);
     return importer.then(config => config.default?.default
         ? config.default[profile]
