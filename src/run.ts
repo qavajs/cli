06325dd7f3c0b75c20b74e468e5822120fb7890d
AlexandrLegchilov <Alexandr_Legchilov@epam.com>
2023-12-15 19:31:35 +0300

    debug

--- src/run.ts
+++ src/run.ts
@@ -21,7 +21,16 @@ function mergeTags(tags: string[]) {
     return tags.map((tag: string) => `(${tag})`).join(' and ');
 }
 
-export async function run({ runCucumber, loadConfiguration, loadSources }: any, chalk: any): Promise<void> {
+/**
+ * Returns a new promise that is automatically rejected after a delay time
+ * @param delay - number milliseconds to delay rejection with an error
+ * @param timeoutMessage - string message to set as rejection error
+ */
+function getDelayedRejectedPromise(delay: number, timeoutMessage: string): Promise<Error> {
+    return new Promise((_, reject) => setTimeout(() => reject(new Error(timeoutMessage)), delay))
+}
+
+export async function run({runCucumber, loadConfiguration, loadSources}: any, chalk: any): Promise<void> {
     const argv: any = yargs(process.argv).argv;
     process.env.CONFIG = argv.config ?? 'config.js';
     process.env.PROFILE = argv.profile ?? 'default';
@@ -29,12 +38,12 @@ export async function run({ runCucumber, loadConfiguration, loadSources }: any, 
     process.env.CLI_ARGV = process.argv.join(' ');
     const serviceHandler = new ServiceHandler(process.env.CONFIG as string, process.env.PROFILE as string);
     const config = await importConfig(process.env.CONFIG as string, process.env.PROFILE as string);
-    const serviceTimeout = config.serviceTimeout || 600_000
+    const serviceTimeout = config.serviceTimeout ?? 60_000
     const timeoutMessage = `Service timeout '${serviceTimeout}' ms exceeded`;
     process.env.DEFAULT_TIMEOUT = config.defaultTimeout ?? 10_000;
     await Promise.race([
         // @ts-ignore
-        new Promise((_, reject) => setTimeout(reject(new Error(timeoutMessage)), serviceTimeout)),
+        getDelayedRejectedPromise(serviceTimeout, timeoutMessage),
         serviceHandler.before()
     ]);
     const memoryLoadHook = path.resolve(__dirname, './loadHook.js');
@@ -51,30 +60,31 @@ export async function run({ runCucumber, loadConfiguration, loadSources }: any, 
         provided: {...config, ...argv},
         profiles: [process.env.PROFILE as string]
     }
-    const { runConfiguration } = await loadConfiguration(options, environment);
+    const {runConfiguration} = await loadConfiguration(options, environment);
     runConfiguration.support.requireModules = [memoryLoadHook, ...runConfiguration.support.requireModules];
     if (argv.shard) {
         console.log(chalk.blue(`Shard: ${argv.shard}`));
-        const { plan } = await loadSources(runConfiguration.sources);
-        const [ shard, totalShards ] = argv.shard.split('/').map((val: string) => parseInt(val));
+        const {plan} = await loadSources(runConfiguration.sources);
+        const [shard, totalShards] = argv.shard.split('/').map((val: string) => parseInt(val));
         process.env.SHARD = shard;
         process.env.TOTAL_SHARDS = totalShards;
         const chunkLength = plan.length / totalShards;
         const startIndex = Math.floor(shard * chunkLength - chunkLength);
-        const endIndex = totalShards/shard === 1 ? plan.length : chunkLength * shard;
+        const endIndex = totalShards / shard === 1 ? plan.length : chunkLength * shard;
         const chunk = plan.slice(startIndex, endIndex);
         runConfiguration.sources.names = chunk.map((scenario: IPlannedPickle) => scenario.name);
     }
-    const { plan } = await loadSources(runConfiguration.sources);
+    const {plan} = await loadSources(runConfiguration.sources);
     console.log(chalk.blue(`Test Cases: ${plan.length}`));
     const result: IRunResult = await runCucumber(runConfiguration, environment);
     await Promise.race([
         // @ts-ignore
-        new Promise((_, reject) => setTimeout(reject(new Error(timeoutMessage)), serviceTimeout)),
+        getDelayedRejectedPromise(serviceTimeout, timeoutMessage),
         serviceHandler.after(result)
     ]);
 }
-export default async function(): Promise<void> {
+
+export default async function (): Promise<void> {
     const chalk = await chalkModule;
     const cucumber = await import('@cucumber/cucumber/api');
     await run(cucumber, chalk);
--- test/serviceTimeout.spec.ts
+++ test/serviceTimeout.spec.ts
@@ -7,22 +7,32 @@ vi.mock('../src/importConfig');
 
 beforeEach(async () => {
     vi.resetAllMocks();
+    vi.useRealTimers()
 });
 
 test.each([
-    ['default', {}, `Service timeout '600000' ms exceeded`],
-    ['custom', {serviceTimeout: 333333}, `Service timeout '333333' ms exceeded`],
-])('%s service timeout', async (_, timeoutValue, errMsg) => {
+    // ['default', {}, `Service timeout '600000' ms exceeded`],
+    ['custom', {serviceTimeout: 60_000, service: [{before() {return new Promise(resolve => setTimeout(()=> resolve(0), 25_000));},
+        }]
+    }, `Service timeout '10000' ms exceeded`, 3_000],
+])('%s service timeout', async (_, timeoutValue, errMsg, msRewind) => {
     vi.mocked(importConfig).mockReturnValue(Promise.resolve(timeoutValue));
     const cucumberMock = {
         runCucumber: vi.fn(),
         loadConfiguration: vi.fn(() => {
-            return {runConfiguration: {support: {requireModules: []}}}
+            return { runConfiguration: { support: { requireModules: [] }}}
         }),
         loadSources: vi.fn(() => {
-            return {plan: []}
+            return { plan: [] }
         }),
     };
-    const chalkMock = {blue: vi.fn()};
-    expect(async () => await run(cucumberMock, chalkMock)).rejects.toThrowError(errMsg);
+    const chalkMock = { blue: vi.fn() };
+    vi.useFakeTimers();
+    // console.log('isFake?:', vi.isFakeTimers())
+
+    const failedRun = run(cucumberMock, chalkMock);
+    vi.advanceTimersByTime(70_000)
+    // const expectation = expect(async () => await run(cucumberMock, chalkMock)).rejects;
+    await expect(failedRun).rejects.toThrowError(errMsg)
+    // await expectation.toThrowError(errMsg)
 });
