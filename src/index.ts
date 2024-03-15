f1802389cf19f53c78a17d7861455b042a6808bb
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2024-03-15 18:14:09 +0200

    fixed main

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,7 @@ const argv: any = cliOptions(process.argv.slice(2));
 async function main() {
     const { bold, cyan } = await chalk;
     console.log(bold(cyan(`@qavajs (v${require('../package.json').version})`)));
-    switch (argv._[0]) {
-        case 'run': return run();
-        default: return run();
-    }
+    return run();
 }
 
 main();
