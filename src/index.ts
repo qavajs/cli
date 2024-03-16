b5dda493cd9cac2596ba27a55355b4764ec7b2b3
Alexander Galichenko <alexandr.galichenko@gmail.com>
2024-03-16 16:46:15 +0200

    install script moved to separate module @qavajs/create (#122)
    * install script moved to separate module @qavajs/create
    make run command optional

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import install from './install';
 import run from './run';
 import { cliOptions } from './cliOptions';
 const chalk = import('chalk').then(m => m.default);
@@ -8,12 +7,7 @@ const argv: any = cliOptions(process.argv.slice(2));
 async function main() {
     const { bold, cyan } = await chalk;
     console.log(bold(cyan(`@qavajs (v${require('../package.json').version})`)));
-    if (!argv._[0]) console.warn('Specify command: install or run');
-    switch (argv._[0]) {
-        case 'install': return install();
-        case 'run': return run();
-        default: console.warn('Command is not supported');
-    }
+    return run();
 }
 
 main();
