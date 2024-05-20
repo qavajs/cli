27f85c919a32bd18184c8fdfccbe826c75813e2e
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2024-05-20 15:04:56 +0300

    - :rocket: added _setValue_ and _getValue_ world functions to interact with memory - :x: - qavajs entities moved from global to cucumber world. globals will be removed in future releases.

--- src/load.ts
+++ src/load.ts
@@ -1,6 +1,7 @@
-import {Before, DataTable, IWorld, setDefaultTimeout, supportCodeLibraryBuilder} from '@cucumber/cucumber';
+import { Before, DataTable, IWorld, setDefaultTimeout, supportCodeLibraryBuilder } from '@cucumber/cucumber';
 import memory from '@qavajs/memory';
 import importConfig from './importConfig';
+import { IQavajsWorld } from './IQavajsWorld';
 
 declare global {
   // eslint-disable-next-line no-var
@@ -26,19 +27,30 @@ export async function executeStep(this: any, text: string, extraParam?: DataTabl
   }
 }
 
+function setValue(key: string, value: any): void {
+  memory.setValue(key, value);
+}
+
+async function getValue(expression: string): Promise<any> {
+  return memory.getValue(expression);
+}
+
 /**
  * Basic initialization hook
  */
-Before({name: 'qavajs init'}, async function (this: IWorld, scenario) {
+Before({name: 'qavajs init'}, async function (this: IQavajsWorld, scenario) {
   process.env.CURRENT_SCENARIO_NAME = scenario.pickle.name;
-  global.config = await config;
-  global.config.memory = global.config.memory ?? [];
-  const memoryInstances = Array.isArray(global.config.memory) ? global.config.memory : [global.config.memory];
+  this.config = await config;
+  this.config.memory = this.config.memory ?? [];
+  const memoryInstances = Array.isArray(this.config.memory) ? this.config.memory : [this.config.memory];
   if (memory.setLogger) {
     memory.setLogger(this);
   }
   memory.register(Object.assign({}, ...memoryInstances, memoryValues));
   this.executeStep = executeStep;
+  this.getValue = getValue;
+  this.setValue = setValue;
+  global.config = this.config;
 });
 
 setDefaultTimeout(parseInt(process.env.DEFAULT_TIMEOUT as string));
